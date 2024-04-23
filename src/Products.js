c6c91416cba2f3f72ec415c261ba55bc51f7b5f5
AtroobaIqbal <atroobaiqbal28@gmail.com>
2024-04-23 23:53:21 +0500

    first commit

---
+++ src/Products.js
@@ -0,0 +1,54 @@
+import product1 from "./assets/2149509248.jpg"
+
+
+
+export const PRODUCTS = [
+    {
+        id : 1,
+        productName : "diamond ring",
+        price : 999.0,
+        productImage : product1
+    },
+    {
+        id : 2,
+        productName : "diamond ring",
+        price : 999.0,
+        productImage : product1
+    },
+    {
+        id : 3,
+        productName : "diamond ring",
+        price : 999.0,
+        productImage : product1
+    },
+    {
+        id : 4,
+        productName : "diamond ring",
+        price : 999.0,
+        productImage : product1
+    },
+    {
+        id : 5,
+        productName : "diamond ring",
+        price : 999.0,
+        productImage : product1
+    },
+    {
+        id : 6,
+        productName : "diamond ring",
+        price : 999.0,
+        productImage : product1
+    },
+    {
+        id : 7,
+        productName : "diamond ring",
+        price : 999.0,
+        productImage : product1
+    },
+    {
+        id : 8,
+        productName : "diamond ring",
+        price : 999.0,
+        productImage : product1
+    }
+]
